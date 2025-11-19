4954bbdad4a91a3f72590e395c4b56b5d5f6c87f
Andrew Smith <meuzishun@gmail.com>
2025-11-18 19:32:03 -0500

    Add Map component to map page

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,18 +1,20 @@
-import { chinguService } from '../../features/chingu/chingu.service';
-import { ChinguCountryStats } from '../../features/chingu/chingu.type';
+// import { chinguService } from '../../features/chingu/chingu.service';
+// import { ChinguCountryStats } from '../../features/chingu/chingu.type';
+import Map from './Map';
 
 export default async function MapPage() {
-  const countries = await chinguService.getCountsByCountry();
+  // const countries = await chinguService.getCountsByCountry();
 
   return (
-    <div>
-      <h1>Map Page</h1>
+    <div className='flex-1'>
+      <Map />
+      {/* <h1>Map Page</h1>
       {countries.map((country: ChinguCountryStats) => (
         <div key={country.countryCode} className='flex gap-3'>
           <p>{country.countryName} ({country.countryCode})</p>
           <p>{country.count}</p>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
