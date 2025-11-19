263eaa7811cec2ff4cea8deebfa482e39c4bc59a
Andrew Smith <meuzishun@gmail.com>
2025-11-19 16:00:29 -0500

    Add MobileNav to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,15 @@
-import Logo from './Logo'
-import Navbar from './Navbar'
+import Logo from './Logo';
+import MobileNav from './MobileNav';
+import Navbar from './Navbar';
 
 function Header() {
   return (
-    <header className='flex flex-row justify-between align-center py-3 px-5'>
+    <header className="flex flex-row justify-between align-center py-3 px-5">
       <Logo />
-      <Navbar/>
+      <MobileNav />
+      <Navbar />
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
