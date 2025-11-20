5c6ee9843a04698e893a955dc29cd468d35a9647
Andrew Smith <meuzishun@gmail.com>
2025-11-20 10:54:29 -0500

    Create route for country stats

---
+++ src/app/api/chingu/country-stats/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { chinguController } from '@/features/chingu/chingu.controller';
+
+export async function GET(req: NextRequest) {
+  try {
+    const data = await chinguController.countryStats();
+    return NextResponse.json(data);
+  } catch (e) {
+    const err = e as Error;
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
