2f89f5f5802fa73b0c0deb0a42d2e9c94ed33a6b
Andrew Smith <meuzishun@gmail.com>
2025-11-23 17:08:10 -0500

    Clean ListPage

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,14 +1,16 @@
 import { chinguService } from '../../features/chingu/chingu.service';
 import { ChinguType } from '../../features/chingu/chingu.type';
-import { columns } from "./columns"
-import { DataTable } from "./data-table"
+import { columns } from './columns';
+import { DataTable } from './data-table';
 
 export default async function ListPage() {
-  const chingus: ChinguType[] = await chinguService.getAllChingus({ limit: 50 });
+  const chingus: ChinguType[] = await chinguService.getAllChingus({
+    limit: 50,
+  });
 
   return (
     <div className="container mx-auto py-10">
       <DataTable columns={columns} data={chingus} />
     </div>
-  )
+  );
 }
