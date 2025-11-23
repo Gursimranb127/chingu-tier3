d734ac4fac3c733fbc366eeacb819cd45561cf37
Andrew Smith <meuzishun@gmail.com>
2025-11-22 23:21:04 -0500

    Add margin for large screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex-1 flex align-center justify-center justify-items-center bg-[url('/images/world1.jpg')] bg-cover bg-top">
       <div className="flex flex-col gap-10 justify-start w-65 m-20">
-        <h1 className="text-4xl text-center text-white mt-10 md:mt-20">
+        <h1 className="text-4xl text-center text-white mt-10 md:mt-20 lg:mt-30">
           Find your fellow Chingus around the world
         </h1>
         <Link href="/map" className="self-center">
